9e275ffe5548ffdd02f8e4c887497c5e68c6f544
schanthery <schanthery@yahoo.fr>
2021-05-31 10:23:39 +0200

    Add files via upload

---
+++ programme 1.py
@@ -0,0 +1,7 @@
+from math import *
+
+def fonction2(a):
+    resultat = sqrt(a)
+    return resultat
+
+print(fonction2(25))
---
+++ programme 2.py
@@ -0,0 +1,8 @@
+from math import *
+
+def milieu(r,s,t,u):
+    x=(r+s)/2
+    y=(t+u)/2
+    return (x,y)
+
+print(milieu(25,10,20,36))
